29abefaf51e63b64827b07dd51fcf204ac8cf6ca
kimzro <vkzlzmffjq@gmail.com>
2018-07-20 17:44:32 +0900

    ~#11

--- src/App.js
+++ src/App.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title:"Matrix",
-    poster:"http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
-  },
-  {
-    title:"Full Metal Jacket",
-    poster: "http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
-  },
-  {
-    title:"Old boy",
-    poster: "http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
-  },
-  {
-    title:"Star Wars",
-    poster: "http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
-  }
-]
+
 
 class App extends Component {
+  state = {
+    greeting: 'Hello!',
+    movies : [
+      {
+        title:"Matrix",
+        poster:"http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
+      },
+      {
+        title:"Full Metal Jacket",
+        poster: "http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
+      },
+      {
+        title:"Old boy",
+        poster: "http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
+      },
+      {
+        title:"Star Wars",
+        poster: "http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
+      }
+    ]
+  }
+
+
   componentWillMount(){
 
   }
@@ -29,7 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {movies.map((movies, index) =>{
+        {this.state.movies.map((movies, index) =>{
           return <Movie title={movies.title} poster={movies.poster} key={index} />
         })}
       </div>
@@ -37,8 +43,19 @@ class App extends Component {
   }
 
   componentDidMount(){
-    
+    setTimeout(()=>{
+      this.setState({
+        movies: [
+          ...this.state.movies, //이전영화리스트 그대로 두고 뒤에 추가.
+          {
+            title: "Transporting",
+            poster: "http://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/movie/ea2a05b045324e4e872d6b7ea0462592.jpg"
+          }
+        ]
+      })
+    }, 5000)
   }
+
 }
 
 export default App;
